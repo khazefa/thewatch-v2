1822dc4b5306d2580675660bc5da64fcd1cb8c01
Sigit Prayitno <cybergitt@gmail.com>
2019-07-09 14:57:57 +0700

    Update about.php

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -10,6 +10,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
+	<h2><?= Html::encode($url_name) ?></h2>
     <p>This is the About page. You may modify the following file to customize its content:</p>
 
     <code><?= __FILE__ ?></code>
