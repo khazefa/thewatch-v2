af09e32a3e8e78fdc0a46e17b78a451e7cf5a097
Sigit Prayitno <cybergitt@gmail.com>
2019-10-04 14:49:56 +0700

    Update files

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -6,14 +6,22 @@ $params = array_merge(
     require __DIR__ . '/params-local.php'
 );
 
+$home_url = $_SERVER['HTTP_HOST'] === 'localhost' ? '/thewatch-v2' : '/';
+$base_url = $_SERVER['HTTP_HOST'] === 'localhost' ? '/thewatch-v2' : '';
+
 return [
     'id' => 'app-frontend',
+    'name' => 'The Watch Co.',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'timeZone' => 'Asia/Jakarta',
     'controllerNamespace' => 'frontend\controllers',
+    'homeUrl' => $home_url, // hosted home url
     'components' => [
         'request' => [
-            'csrfParam' => '_csrf-frontend',
+            'baseUrl'=>$base_url, // hosted base url
+			'class' => 'common\components\Request',
+			'web'=> '/frontend/web'
         ],
         'user' => [
             'identityClass' => 'common\models\User',
